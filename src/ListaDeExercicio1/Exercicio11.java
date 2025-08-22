bf19774fb1154b5767908e445de8fd272a63a6c4
Lucas Silva Sarmento <lucassilvasarmentos3@email.com>
2025-08-22 13:40:35 -0300

    Atividade

--- src/ListaDeExercicio1/Exercicio11.java
+++ src/ListaDeExercicio1/Exercicio11.java
@@ -9,8 +9,33 @@ public class Exercicio11 {
         System.out.println("ENTRE COM DADOS DA ALTURA : ");
         double altura = scan.nextDouble();
 
-        System.out.println("ENTE COM OS DADOS DO SEXO (H/M): ");
-        var sexoMasculino = scan.next().equalsIgnoreCase("h");
+        System.out.println("Entre com os dados do seu sexo (H/M): ");
+        var sexo = scan.next();
+
+        System.out.println("Entre com seu peso em (KG): ");
+        var pesoAtual = scan.nextDouble();
+
+        double pesoIdeal;
+
+        if (sexo.equalsIgnoreCase("H")) {
+          pesoIdeal = (72.7 * altura) - 58;
+        } else if (sexo.equalsIgnoreCase("M")) {
+            pesoIdeal = (62.1 * altura) - 44.7;
+        } else {
+            System.out.println("Sexo inválido!");
+            scan.close();
+            return;
+        }
+
+        System.out.printf("Seu peso ideal é: %.2f kg%n", pesoIdeal);
+
+        if (pesoAtual > pesoIdeal) {
+            System.out.println("Você está acima do peso ideal.");
+        } else if (pesoAtual < pesoIdeal) {
+            System.out.println("Você está abaixo do peso ideal.");
+        } else {
+            System.out.println("Você está no peso ideal!");
+        }
 
         scan.close();
     }
---
+++ src/ListaDeExercicio1/Exercicio12.java
@@ -0,0 +1,11 @@
+package ListaDeExercicio1;
+
+import java.util.Scanner;
+
+public class Exercicio12 {
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in);
+        
+        scan.close();
+    }
+}
